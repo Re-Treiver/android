f5b60c2954e17f4a7de12cc6ff084488151a463f
ahyeonkong <kimah0403@gmail.com>
2024-05-10 04:17:04 +0900

    사진 촬영 및 서버 연동 완성

--- app/src/main/java/com/example/re_treiver/MainActivity.java
+++ app/src/main/java/com/example/re_treiver/MainActivity.java
@@ -1,23 +1,40 @@
 package com.example.re_treiver;
 
+
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.IOException;
+import com.bumptech.glide.Glide;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, View.OnTouchListener {
 
     private static final int REQUEST_CAMERA_PERMISSION = 200;
@@ -25,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private SurfaceView mSurfaceView;
     private SurfaceHolder mSurfaceHolder;
     private long lastTouchTime = 0;
+    private Button mUploadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +50,22 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         mSurfaceView = findViewById(R.id.cameraView);
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
         mSurfaceView.setOnTouchListener(this);
+
+        mUploadButton = findViewById(R.id.uploadButton);
+        mUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCamera != null) {
+                    takePicture();
+                }
+            }
+        });
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -59,59 +86,131 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         }
         return true;
     }
+
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        //만들어지는시점
-        mCamera  = Camera.open();//카메라 객체 참조
-        try{
-            mCamera.setPreviewDisplay(mSurfaceHolder);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        mCamera = openCamera(surfaceHolder);
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-        //변경
-        mCamera.startPreview(); //렌즈로 부터 들어오는 영상을 뿌려줌
-        mCamera.stopPreview();
-        mCamera.setDisplayOrientation(90);//카메라 미리보기 오른쪽 으로 90 도회전
-        mCamera.startPreview();
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        if (mSurfaceHolder.getSurface() == null) {
+            return;
+        }
 
+        try {
+            // Only stop preview if the surface size has changed significantly
+            if (width != mSurfaceView.getWidth() || height != mSurfaceView.getHeight()) {
+                mCamera.stopPreview();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+            mCamera.setDisplayOrientation(90);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        //소멸
-        mCamera.stopPreview();//미리보기중지
+        mCamera.stopPreview();
         mCamera.release();
         mCamera = null;
     }
 
-    private void openCamera(SurfaceHolder holder) {
+    private void takePicture() {
+        if (mCamera != null) {
+            mCamera.takePicture(null, null, new Camera.PictureCallback() {
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    File pictureFile = getOutputMediaFile();
+                    if (pictureFile != null) {
+                        try {
+                            FileOutputStream fos = new FileOutputStream(pictureFile);
+
+                            // 이미지 회전 처리
+                            ExifInterface exifInterface = new ExifInterface(pictureFile.getAbsolutePath());
+                            int orientation = exifInterface.getAttributeInt(
+                                    ExifInterface.TAG_ORIENTATION,
+                                    ExifInterface.ORIENTATION_NORMAL);
+                            int rotation = 90;
+                            switch (orientation) {
+                                case ExifInterface.ORIENTATION_ROTATE_90:
+                                    rotation = 180;
+                                    break;
+                                case ExifInterface.ORIENTATION_ROTATE_180:
+                                    rotation = 270;
+                                    break;
+                                case ExifInterface.ORIENTATION_ROTATE_270:
+                                    rotation = 0;
+                                    break;
+                            }
+                            exifInterface.setAttribute(ExifInterface.TAG_ORIENTATION, Integer.toString(rotation));
+
+                            fos.write(getRotatedData(data, rotation));
+                            fos.close();
+
+                            // 이미지 파일을 서버로 업로드
+                            FileUploadUtils.send2Server(pictureFile);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    // 사진 찍은 후에는 다시 미리보기를 시작한다
+                    mCamera.startPreview();
+                }
+            });
+        }
+    }
+
+    // 이미지 데이터 회전
+    private byte[] getRotatedData(byte[] data, int rotation) {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+        Bitmap rotatedBitmap = getRotatedBitmap(bitmap, rotation);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+        return outputStream.toByteArray();
+    }
+
+    // 이미지 회전하기
+    private Bitmap getRotatedBitmap(Bitmap bitmap, int degrees) {
+        if (bitmap == null) return null;
+        if (degrees == 0) return bitmap;
+        Matrix matrix = new Matrix();
+        matrix.setRotate(degrees, bitmap.getWidth() / 2f, bitmap.getHeight() / 2f);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "re_treiver");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator + "re_treiver_" + timeStamp + ".jpg");
+    }
+
+    private Camera openCamera(SurfaceHolder holder) {
+        Camera camera = null;
         try {
-            mCamera = Camera.open();
-            if (mCamera != null) {
-                mCamera.setPreviewDisplay(holder);
-                mCamera.startPreview();
+            camera = Camera.open();
+            if (camera != null) {
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void releaseCamera() {
-        if (mCamera != null) {
-            mCamera.stopPreview();
-            mCamera.release();
-            mCamera = null;
-        }
-    }
-
-    private void takePicture() {
-        if (mCamera != null) {
-            mCamera.takePicture(null, null, null);
-        }
+        return camera;
     }
 
     @Override
